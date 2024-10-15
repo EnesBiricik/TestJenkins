1e08ac9ac9b51edeaf21ea8638a9fcc721738bb6
EnesBiricik <aslan200313@gmail.com>
2024-10-15 22:09:05 +0300

    new test

--- DockerTest1/Controllers/WeatherForecastController.cs
+++ DockerTest1/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace DockerTest1.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "FreezingXXXX", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "FreezingXXXX", "Braasdasdasdcing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
