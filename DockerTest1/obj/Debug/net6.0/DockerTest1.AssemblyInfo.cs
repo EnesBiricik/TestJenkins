00b5bb39fa099abcaf9111173b792e7f1d3a6e9f
EnesBiricik <aslan200313@gmail.com>
2024-10-16 21:25:21 +0300

    update compose

--- DockerTest1/obj/Debug/net6.0/DockerTest1.AssemblyInfo.cs
+++ DockerTest1/obj/Debug/net6.0/DockerTest1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DockerTest1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e08ac9ac9b51edeaf21ea8638a9fcc721738bb6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dbe8be6f802c52e8e45f7b28f56b1a313e54be8c")]
 [assembly: System.Reflection.AssemblyProductAttribute("DockerTest1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DockerTest1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
