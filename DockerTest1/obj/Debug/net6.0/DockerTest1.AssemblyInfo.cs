214007101ec1386dad67d1d97dc9ad0e473ca94a
EnesBiricik <aslan200313@gmail.com>
2024-10-17 14:48:09 +0300

    / added dockerfile

--- DockerTest1/obj/Debug/net6.0/DockerTest1.AssemblyInfo.cs
+++ DockerTest1/obj/Debug/net6.0/DockerTest1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DockerTest1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e886b53dcaf95d75776ae58e19b18c80f8771f3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af71ef40ce36b6efcd6c64fdf80956512217c6bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("DockerTest1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DockerTest1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
