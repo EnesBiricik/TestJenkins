3c4fd13a7577f51aaf5265798829839bc1901a2d
EnesBiricik <aslan200313@gmail.com>
2024-10-17 23:13:32 +0300

    retest trigger

--- DockerTest1/Controllers/WeatherForecastController.cs
+++ DockerTest1/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace DockerTest1.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "FreezingSalih", "Braas", "ChillyRecep", "CoolEnes", "MildÖmer", "CoolEkrem", "BalmyVeli", "HotBurak", "Sweltering", "Scorching"
+            "FreezingSalih", "Braas", "ChillyRecep", "CoolEnes", "MildÖmer", "CoolEkrem", "BalmyVeli", "HotBurak", "SwelteringOuz", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
