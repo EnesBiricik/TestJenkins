93df8c17f3ea5da8f98eef9edad2879343e99ec7
EnesBiricik <aslan200313@gmail.com>
2024-10-17 22:13:15 +0300

    test webhook trigger

--- DockerTest1/Controllers/WeatherForecastController.cs
+++ DockerTest1/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace DockerTest1.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "FreezingXXXX", "Braasdasdasdcing", "ChillyZZZZZZZZZZZZZZZZ", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Braas", "ChillyRecep", "CoolEnes", "Mild", "CoolEkrem", "BalmyVeli", "HotBurak", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
